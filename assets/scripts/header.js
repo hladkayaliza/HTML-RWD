038f64c4386b89e2ef05181836871af55292a9a0
Liza Hladkaya <liza.hladkaya@gmail.com>
2021-12-27 16:30:46 +0300

    Add header + adaptive

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,10 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const navItems = document.querySelector(".navbar__content");
 const navLink = document.querySelectorAll(".nav-link");
+const userLink = document.querySelectorAll(".navbar__user-link");
 
 
 hamburger.addEventListener("click", mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
+userLink.forEach(n => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
     hamburger.classList.remove("active");
