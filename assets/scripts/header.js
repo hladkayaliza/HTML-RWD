e7a9888f4d8672ca801ee1e5655cb2d4d8290a91
Lizaveta Hladkaya <Lizaveta.Hladkaya@itechart-group.com>
2021-12-29 16:51:40 +0300

    Refactoring header

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,12 +1,14 @@
-const hamburger = document.querySelector(".hamburger");
-const navItems = document.querySelector(".navbar__content");
+const hamburger = document.querySelector(".navbar-burger");
+const navItems = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
 const userLink = document.querySelectorAll(".navbar__user-link");
+const searchForm = document.querySelectorAll(".search-form");
 
 
 hamburger.addEventListener("click", mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 userLink.forEach(n => n.addEventListener("click", closeMenu));
+searchForm.addEventListener()
 
 function closeMenu() {
     hamburger.classList.remove("active");
